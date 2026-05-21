05e264ece94eeee588af4f55bfc78c97ae803170
Richard2851 <danielricardolinares28@gmail.com>
2026-05-21 09:52:09 -0500

    Add files via upload

---
+++ JBRD_2_0/php/JBRD.php
@@ -0,0 +1,76 @@
+<?php
+$conexion = mysqli_connect(
+    "localhost",
+    "root",
+    "",
+    "jbrd"
+);
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+
+if (isset($_POST['Registrar'])) {
+
+    // DATOS PERSONALES
+    $nombres = $_POST['nombre'];
+    $apellidos = $_POST['apellido'];
+    $tipo_documento = $_POST['tipo_documento'];
+    $documento = $_POST['documento'];
+    $correo = $_POST['correo'];
+    $contacto = $_POST['telefono'];
+    $fecha_nac = $_POST['fecha_nac'];
+    $fecha_exp = $_POST['fecha_exp'];
+
+    // INSERTAR EN MYSQL
+    $sql = "INSERT INTO usuarios (
+
+        nombre,
+        apellido,
+        tipo_documento,
+        documento,
+        correo,
+        telefono,
+        fecha_nac,
+        fecha_exp
+
+    ) VALUES (
+
+        '$nombres',
+        '$apellidos',
+        '$tipo_documento',
+        '$documento',
+        '$correo',
+        '$contacto',
+        '$fecha_nac',
+        '$fecha_exp'
+
+    )";
+
+    // EJECUTAR CONSULTA
+    mysqli_query($conexion, $sql);
+
+    // REDIRECCIONAR
+    header("Location: ../html/registro.php");
+
+    exit();
+}
+if (isset($_POST['volver'])) {
+
+    $usuario = $_POST['Usuario'];
+    $password = $_POST['Password'];
+
+    $vreg = "INSERT INTO cuenta (Usuario , Password) VALUES ('$usuario','$password')";
+
+    mysqli_query($conexion, $vreg);
+
+    header("Location: ../html/login.php");
+
+    exit();
+
+
+}
+
+
+
+
+?>
